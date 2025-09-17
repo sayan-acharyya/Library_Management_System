4b8e59cc4b4c2032ed9d53539a8b56e3d794d17c
sayan-acharyya <acharyyas735@gmail.com>
2025-09-17 10:42:20 +0530

    update

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -129,7 +129,9 @@ export const login = catchAsyncErrors(async (req, res, next) => {
 export const logout = catchAsyncErrors(async (req, res, next) => {
     res.status(200).cookie("token", "", {
         expires: new Date(Date.now()),
-        httpOnly: true
+        httpOnly: true,
+        secure:true,
+        sameSite:"None",
     }).json({
         success: true,
         message: "Logged out successfully",
--- utils/sendToken.js
+++ utils/sendToken.js
@@ -6,13 +6,15 @@
 export const sendToken = (user, statusCode, message, res) => {
 
     const token = user.generateToken();
-    res.status(statusCode).cookie("token",token, {
+    res.status(statusCode).cookie("token", token, {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly:true,
+        httpOnly: true,
+        secure: true,
+        sameSite: "None"
     }).json({
-        success:true,
+        success: true,
         user,
         message,
         token,
